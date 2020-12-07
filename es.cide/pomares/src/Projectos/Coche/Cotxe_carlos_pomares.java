c83c20ed96f4721f67a35d92902f12239777e463
Carlos Pomares <45567247+pomaretta@users.noreply.github.com>
2020-12-07 19:04:54 +0100

    Added some functionalities and the preview version of Car management.

--- es.cide/pomares/src/Projectos/Coche/Cotxe_carlos_pomares.java
+++ es.cide/pomares/src/Projectos/Coche/Cotxe_carlos_pomares.java
@@ -13,10 +13,385 @@ package Projectos.Coche;
     
 */
 
+import java.util.HashMap;
+
 /**
  * @author Carlos Pomares
  */
 
 
-public class Cotxe_carlos_pomares {
+public class Cotxe_carlos_pomares extends CotxeAbstracte {
+
+    // Constants
+    final private static int MAX_VELOCITAT = 250;
+    final private static int VELOCITAT_INCREMENTADOR = 5;
+    final private static int REVOLUCIONS_ENMARXA = 930;
+    final private static int MAX_MARXASECUENCIAL = 7;
+    final private boolean DESCAPOTABLE;
+
+    // Vehicle individual
+    private int revolucions = 1;
+    private int velocitat = 0;
+    private boolean reverse;
+    private boolean capota;
+    private boolean cinturo;
+    private EstatsMotorCotxe estatCoxte = EstatsMotorCotxe.Aturat;
+    private int marxaActual = 0;
+
+    // Components
+    final private HashMap<String,Integer> components = new HashMap<>();
+
+    public Cotxe_carlos_pomares(String marca, String model, TipusCanvi tipuscanvi, boolean descapotable) {
+        super(marca,model,tipuscanvi);
+        this.DESCAPOTABLE = descapotable;
+
+        components.put("Aire acondicionat",0);
+        if(DESCAPOTABLE){
+            components.put("Capota",0);
+        }
+        components.put("Limpia parabrisas",0);
+
+    }
+    public void accelerar() throws Exception {
+        if(this.velocitat >= 0 && this.velocitat <= MAX_VELOCITAT){
+            if(this.tipuscanvi == TipusCanvi.CanviManual){
+                switch (this.marxaActual){
+                    case 0 -> {
+                        if(this.reverse){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesita poner una marxa.");
+                        }
+                    }
+                    // First gear
+                    case 1 -> {
+                        if(this.velocitat <= 10){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesites aumentar marxa.");
+                        }
+                    }
+                    case 2 -> {
+                        if(this.velocitat <= 25){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesites aumentar marxa.");
+                        }
+                    }
+                    case 3 -> {
+                        if(this.velocitat <= 50){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesites aumentar marxa.");
+                        }
+                    }
+                    case 4 -> {
+                        if(this.velocitat <= 75){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesites aumentar marxa.");
+                        }
+                    }
+                    case 5 -> {
+                        if(this.velocitat <= 100){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesites aumentar marxa.");
+                        }
+                    }
+                    case 6 -> {
+                        if(this.velocitat <= 145){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesites aumentar marxa.");
+                        }
+                    }
+                    case 7 -> {
+                        if(this.velocitat <= (MAX_VELOCITAT - 5)){
+                            incrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Necesites aumentar marxa.");
+                        }
+                    }
+                    default -> throw new RuntimeException("Error");
+
+                }
+            } else if(this.tipuscanvi == TipusCanvi.CanviAutomatic){
+                if(this.estatCoxte == EstatsMotorCotxe.EnMarxa){
+                    if(this.velocitat == 0 && this.marxaActual == 0){
+                        this.incrementarMarxa();
+                        this.incrementarVelocitat();
+                    } else if(this.marxaActual >= 1 && this.marxaActual <= MAX_MARXASECUENCIAL){
+                        switch (this.velocitat){
+                            case 15, 25, 50, 80, 90, 120 -> {
+                                incrementarMarxa();
+                                incrementarVelocitat();
+                            }
+                            default -> incrementarVelocitat();
+                        }
+                    }
+                }
+            } else {
+                throw new Exception("Error.");
+            }
+        } else {
+            throw new Exception("No se pot accelerar.");
+        }
+    }
+    public void frenar() throws Exception {
+        if(this.velocitat >= 5){
+            if(this.tipuscanvi == TipusCanvi.CanviAutomatic){
+                switch (this.marxaActual){
+                    case 0 -> {
+                        if(reverse){
+                            decrementarVelocitat();
+                        } else {
+                            throw new RuntimeException("Error");
+                        }
+                    }
+                    case 2 -> {
+                        if(this.velocitat <= 10) {
+                            decrementarMarxa();
+                        }
+                        decrementarVelocitat();
+                    }
+                    case 3 -> {
+                        if(this.velocitat <= 20) {
+                            decrementarMarxa();
+                        }
+                        decrementarVelocitat();
+                    }
+                    case 4 -> {
+                        if(this.velocitat <= 40) {
+                            decrementarMarxa();
+                        }
+                        decrementarVelocitat();
+                    }
+                    case 5 -> {
+                        if(this.velocitat <= 60) {
+                            decrementarMarxa();
+                        }
+                        decrementarVelocitat();
+                    }
+                    case 6 -> {
+                        if(this.velocitat <= 80) {
+                            decrementarMarxa();
+                            decrementarVelocitat();
+                        }
+                    }
+                    case 7 -> {
+                        if(this.velocitat <= 110) {
+                            decrementarMarxa();
+                        }
+                        decrementarVelocitat();
+                    }
+                    default -> decrementarVelocitat();
+                }
+            } else {
+                decrementarVelocitat();
+            }
+        } else {
+            throw new RuntimeException("Ya estas aturat.");
+        }
+    }
+    private boolean pucPosarMarxaEnrrera() throws Exception {
+        if(this.velocitat <= 5 && this.marxaActual <= 1){
+            return true;
+        } else {
+            throw new RuntimeException("No pots posar marxa enrrera.");
+        }
+    }
+    public void incrementarMarxa() throws Exception {
+        if(this.marxaActual <= MAX_MARXASECUENCIAL) {
+            if(this.reverse){
+                if(this.velocitat == 0 || (this.velocitat >= 0 && this.velocitat <= 5)){
+                    this.marxaActual += 1;
+                }
+            } else {
+                switch (this.marxaActual) {
+                    case 0 -> {
+                        if(this.velocitat == 0){
+                            this.marxaActual += 1;
+                        } else {
+                            throw new RuntimeException("Error.");
+                        }
+                    }
+                    case 1 -> {
+                        if(this.velocitat >= 5 && this.velocitat <= 15){
+                            this.marxaActual += 1;
+                        } else {
+                            throw new RuntimeException("Velocitat insuficient.");
+                        }
+                    }
+                    case 2 -> {
+                        if(this.velocitat >= 20 && this.velocitat <= 30){
+                            this.marxaActual += 1;
+                        } else {
+                            throw new RuntimeException("Velocitat insuficient.");
+                        }
+                    }
+                    case 3 -> {
+                        if(this.velocitat >= 35 && this.velocitat <= 50){
+                            this.marxaActual += 1;
+                        } else {
+                            throw new RuntimeException("Velocitat insuficient.");
+                        }
+                    }
+                    case 4 -> {
+                        if(this.velocitat >= 60 && this.velocitat <= 80){
+                            this.marxaActual += 1;
+                        } else {
+                            throw new RuntimeException("Velocitat insuficient.");
+                        }
+                    }
+                    case 5 -> {
+                        if(this.velocitat >= 90 && this.velocitat <= 110){
+                            this.marxaActual += 1;
+                        } else {
+                            throw new RuntimeException("Velocitat insuficient.");
+                        }
+                    }
+                    case 6 -> {
+                        if(this.velocitat >= 120 && this.velocitat <= 140){
+                            this.marxaActual += 1;
+                        } else {
+                            throw new RuntimeException("Velocitat insuficient.");
+                        }
+                    }
+                    case 7 -> throw new RuntimeException("Máxima marxa.");
+                    default -> throw new RuntimeException("Error.");
+                }
+            }
+        }
+    }
+    public void decrementarMarxa() throws Exception {
+        if(this.marxaActual <= MAX_MARXASECUENCIAL) {
+            switch (this.marxaActual) {
+                case 0 -> {
+                    if(this.velocitat >= 0 && this.velocitat <= 5){
+                        this.reverse = true;
+                    } else {
+                        throw new RuntimeException("Error.");
+                    }
+                }
+                case 1 -> {
+                    if(this.velocitat <= 15){
+                        this.marxaActual -= 1;
+                    } else {
+                        throw new RuntimeException("Molta velocitat.");
+                    }
+                }
+                case 2 -> {
+                    if(this.velocitat <= 25){
+                        this.marxaActual -= 1;
+                    } else {
+                        throw new RuntimeException("Molta velocitat.");
+                    }
+                }
+                case 3 -> {
+                    if(this.velocitat <= 45){
+                        this.marxaActual -= 1;
+                    } else {
+                        throw new RuntimeException("Molta velocitat.");
+                    }
+                }
+                case 4 -> {
+                    if(this.velocitat <= 55){
+                        this.marxaActual -= 1;
+                    } else {
+                        throw new RuntimeException("Molta velocitat.");
+                    }
+                }
+                case 5 -> {
+                    if(this.velocitat <= 70){
+                        this.marxaActual -= 1;
+                    } else {
+                        throw new RuntimeException("Molta velocitat.");
+                    }
+                }
+                case 6 -> {
+                    if(this.velocitat <= 100){
+                        this.marxaActual -= 1;
+                    } else {
+                        throw new RuntimeException("Molta velocitat.");
+                    }
+                }
+                case 7 -> {
+                    if(this.velocitat <= 130){
+                        this.marxaActual -= 1;
+                    } else {
+                        throw new RuntimeException("Molta velocitat.");
+                    }
+                }
+                default -> throw new RuntimeException("Error.");
+            }
+        }
+    }
+    private void incrementarVelocitat(){
+        if(this.velocitat <= MAX_VELOCITAT){
+            this.velocitat += VELOCITAT_INCREMENTADOR;
+            this.revolucions = (REVOLUCIONS_ENMARXA + ((this.velocitat / 10) * this.marxaActual));
+        }
+    }
+    private void decrementarVelocitat(){
+        if(this.velocitat >= 5){
+            this.velocitat -= VELOCITAT_INCREMENTADOR;
+        }
+    }
+    @Override
+    public void arrancarMotor() throws Exception {
+        if(this.estatCoxte == EstatsMotorCotxe.EnMarxa){
+            throw new Exception("Error, es cotxe ja esta en marxa.");
+        } else {
+            this.estatCoxte = EstatsMotorCotxe.EnMarxa;
+        }
+    }
+    @Override
+    public void aturarMotor() throws Exception {
+        if(this.estatCoxte == EstatsMotorCotxe.Aturat){
+            throw new Exception("Error, es cotxe ja esta aturat.");
+        } else {
+            this.estatCoxte = EstatsMotorCotxe.Aturat;
+        }
+    }
+    @Override
+    public EstatsMotorCotxe comprovaMotor() {
+        return this.estatCoxte;
+    }
+    public int getVelocitat(){
+        return this.velocitat;
+    }
+    public String getMarca(){
+        return this.marca;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public String getTipusCanvi(){
+        return this.tipuscanvi.toString();
+    }
+    public HashMap<String,Integer> getComponents() {
+        return components;
+    }
+    public boolean getSiEsDescapotable(){
+        return this.DESCAPOTABLE;
+    }
+    public boolean getSiEsReverse(){
+        return this.reverse;
+    }
+    public int getMarxaActual(){
+        return this.marxaActual;
+    }
+    @Override
+    public int getRevolucions() {
+        if(this.estatCoxte == EstatsMotorCotxe.Aturat){
+            return 0;
+        } else {
+            if(velocitat == 0){
+                return REVOLUCIONS_ENMARXA;
+            } else {
+                return this.revolucions;
+            }
+        }
+    }
 }
--- es.cide/pomares/src/Projectos/Coche/TestCotxe_carlos_pomares.java
+++ es.cide/pomares/src/Projectos/Coche/TestCotxe_carlos_pomares.java
@@ -13,10 +13,308 @@ package Projectos.Coche;
     
 */
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Carlos Pomares
  */
 
 
 public class TestCotxe_carlos_pomares {
+
+    final private static ArrayList<Cotxe_carlos_pomares> COTXES = new ArrayList<>();
+    final private static ArrayList<String> ORDRES = new ArrayList<>();
+    final private static ArrayList<String> ERRORS = new ArrayList<>();
+    final private static Scanner USER_IN = new Scanner(System.in);
+
+    private static Cotxe_carlos_pomares vehicleSeleccionat;
+
+    private static void menuPrincipal(){
+
+        boolean exit = false;
+
+        String[] opcions = new String[]{"Crear vehicle","Generar vehicles","Seleccionar vehicle","Conduir vehicle","Sortir"};
+
+        System.out.printf("========= %s =========\n","Bienvenido");
+
+        while(!exit){
+
+            if(vehicleSeleccionat != null){
+
+                System.out.print("\n\t--------- Vehicle seleccionat ---------\n");
+
+                System.out.printf("\t%-6s %-15s %-10s %-20s %-10s\n",
+                        "Nombre","Marca",
+                        "Model","Tipus canvi",
+                        "Descapotable");
+
+                System.out.printf("\t%-6d %-15s %-10s %-20s %-10b\n",
+                        (COTXES.indexOf(vehicleSeleccionat) + 1),vehicleSeleccionat.getMarca(),
+                        vehicleSeleccionat.getModel(),vehicleSeleccionat.getTipusCanvi(),
+                        vehicleSeleccionat.getSiEsDescapotable());
+
+                System.out.print("\t---------------------------------------\n");
+            }
+
+            System.out.printf("\n\t%-15s %-30s\n",
+                    "Nombre","Opcio");
+
+            for (int i = 0; i < opcions.length ; i++) {
+                if(vehicleSeleccionat != null){
+                    System.out.printf("\t%-15d %-30s\n",(i + 1),opcions[i]);
+                } else {
+                    if(!"Conduir vehicle".equals(opcions[i])){
+                        System.out.printf("\t%-15d %-30s\n",(i + 1),opcions[i]);
+                    }
+                }
+            }
+
+            System.out.print("\n\tOrdre: ");
+
+            try {
+                switch (Integer.parseInt(USER_IN.nextLine())){
+                    case 1 -> crearVehicle();
+                    case 2 -> generarVehicles();
+                    case 3 -> seleccionarVehicle();
+                    case 4 -> {
+                        if(vehicleSeleccionat != null){
+                            conduirVehicle();
+                        }
+                    }
+                    case 5 -> exit = true;
+                    default -> System.out.println("Selecciona una opcio.");
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.print("========= END MAIN MENU =========\n");
+    }
+
+    // TODO Crear vehicle
+    private static void crearVehicle(){
+        System.out.println("No implementat.");
+    }
+
+    // TODO Generar vehicles
+    private static void generarVehicles(){
+        // Default vehicles
+        Cotxe_carlos_pomares lamborghini = new Cotxe_carlos_pomares("Lamborghini","Gallardo",TipusCanvi.CanviAutomatic,false);
+        Cotxe_carlos_pomares fiat = new Cotxe_carlos_pomares("Fiat","500S",TipusCanvi.CanviManual,false);
+        Cotxe_carlos_pomares mercedes = new Cotxe_carlos_pomares("Mercedes","McLaren",TipusCanvi.CanviAutomatic,true);
+        COTXES.add(lamborghini);
+        COTXES.add(fiat);
+        COTXES.add(mercedes);
+    }
+
+    // TODO Seleccio
+    private static void seleccionarVehicle(){
+
+        System.out.print("\n========= Seleccio de vehicle =========\n");
+
+        System.out.printf("\n\t%-6s %-15s %-10s %-20s %-10s\n",
+                "Nombre","Marca",
+                "Model","Tipus canvi",
+                "Descapotable");
+
+        // Mostrar vehicles
+        for(Cotxe_carlos_pomares vehicle : COTXES){
+            System.out.printf("\t%-6d %-15s %-10s %-20s %-10b\n",
+                    (COTXES.indexOf(vehicle) + 1),vehicle.getMarca(),
+                    vehicle.getModel(),vehicle.getTipusCanvi(),
+                    vehicle.getSiEsDescapotable());
+        }
+
+        // Donar seleccio
+        int seleccio;
+
+        System.out.print("\n\tOrdre: ");
+        seleccio = Integer.parseInt(USER_IN.nextLine());
+
+        for (int i = 0; i < COTXES.size(); i++) {
+            if((seleccio - 1) == i){
+                vehicleSeleccionat = COTXES.get(i);
+                ORDRES.clear();
+                ERRORS.clear();
+            }
+        }
+
+        System.out.print("\n========= END SELECT MENU =========\n");
+    }
+
+    // TODO Conduccio
+    private static void conduirVehicle(){
+
+        String[] opcions = new String[]{
+            "Arrancar","Aturar","Accelerar","Frenar","Pujar marxa","Baixar marxa","Configurar vehicle","Borrar ordres i Errors","Sortir"
+        };
+
+        boolean exit = false;
+
+        System.out.print("\n========= Conduccio de Vehicle =========\n");
+
+        while(!exit){
+            try {
+
+                informacioVehicle();
+                informacioEstadistiques();
+                informacioComponents();
+                historialOrdres();
+                if(ERRORS.size() != 0){
+                    informeErrors();
+                }
+
+                // Mostrar opcions
+                for (int i = 0; i < opcions.length ; i++) {
+                    if(vehicleSeleccionat.comprovaMotor() == EstatsMotorCotxe.EnMarxa){
+                        if(!"Arrancar".equals(opcions[i])) {
+                            if(vehicleSeleccionat.tipuscanvi == TipusCanvi.CanviManual){
+                                System.out.printf("\t(%d) %s",
+                                        (i + 1), opcions[i]);
+                            } else {
+                                if(!"Pujar marxa".equals(opcions[i]) && !"Baixar marxa".equals(opcions[i])){
+                                    System.out.printf("\t(%d) %s",
+                                            (i + 1), opcions[i]);
+                                }
+                            }
+
+                        }
+                    } else {
+                        if("Arrancar".equals(opcions[i]) || "Configurar vehicle".equals(opcions[i]) || "Sortir".equals(opcions[i])) {
+                            System.out.printf("\t(%d) %s",
+                                    (i + 1), opcions[i]);
+                        }
+                    }
+
+                }
+
+                System.out.print("\n\n\tOrdre: ");
+
+                switch (Integer.parseInt(USER_IN.nextLine())){
+                    case 1 -> {
+                        ORDRES.add("Arrancar motor.");
+                        vehicleSeleccionat.arrancarMotor();
+                    }
+                    case 2 -> {
+                        ORDRES.add("Aturar.");
+                        vehicleSeleccionat.aturarMotor();
+                    }
+                    case 3 -> {
+                        ORDRES.add("Accelerar.");
+                        vehicleSeleccionat.accelerar();
+                    }
+                    case 4 -> {
+                        ORDRES.add("Frenar.");
+                        vehicleSeleccionat.frenar();
+                    }
+                    case 5 -> {
+                        ORDRES.add("Pujar marxa.");
+                        vehicleSeleccionat.incrementarMarxa();
+                    }
+                    case 6 -> {
+                        ORDRES.add("Baixar marxa.");
+                        vehicleSeleccionat.decrementarMarxa();
+                    }
+                    case 7 -> {
+                        ORDRES.add("Configurar vehicle.");
+                        configuracioVehicle();
+                    }
+                    case 8 -> {
+                        ORDRES.clear();
+                        ERRORS.clear();
+                    }
+                    case 9 -> exit = true;
+                    default -> System.out.println("Opcio incorrecte.");
+                }
+
+            } catch (Exception e){
+                ERRORS.add(e.getMessage());
+            }
+        }
+
+        System.out.print("\n========= END MODULE =========\n");
+    }
+
+    // TODO Configuracio
+    private static void configuracioVehicle(){
+        System.out.println("No implementat.");
+    }
+
+    // TODO INFORMACIONS
+    private static void informacioVehicle(){
+        // Mostrar vehicle
+        // MARCA - MODEL - TIPUS CANVI - DESCAPOTABLE
+        System.out.print("\n\t------------------------ VEHICLE ---------------------------");
+
+        System.out.printf("\n\t%-15s %-15s %-15s %-15s",
+                "MARCA","MODEL",
+                "TIPUS CANVI","DESCAPOTABLE");
+
+        System.out.printf("\n\t%-15s %-15s %-15s %-15b",
+                vehicleSeleccionat.getMarca(),vehicleSeleccionat.getModel(),
+                vehicleSeleccionat.getTipusCanvi(),vehicleSeleccionat.getSiEsDescapotable());
+
+        System.out.print("\n\t------------------------------------------------------------");
+    }
+    private static void informacioEstadistiques(){
+        // Mostrar estadisticas
+        // VELOCITAT - REVOLUCIONS - MARXA ACTUAL - REVERSE
+        System.out.print("\n\t--------------------- ESTADISTIQUES ------------------------");
+
+        System.out.printf("\n\t%-15s %-15s %-15s %-15s",
+                "VELOCITAT","REVOLUCIONS",
+                "MARXA ACTUAL","ESTATUS");
+
+        System.out.printf("\n\t%-15s %-15s %-15s %-15s",
+                vehicleSeleccionat.getVelocitat(),vehicleSeleccionat.getRevolucions(),
+                vehicleSeleccionat.getMarxaActual(),vehicleSeleccionat.comprovaMotor());
+
+        System.out.print("\n\t------------------------------------------------------------");
+    }
+    private static void informacioComponents(){
+        // CONFIGURACIO (SI)
+        // AIRE ACON - VELOCITAT
+        // CAPOTA - PUESTA
+        // LIMPIA - VELOCITAT
+        System.out.print("\n\t---------------------- CONFIGURACIO ------------------------");
+
+        System.out.printf("\n\t%-20s %-15s",
+                "COMPONENT","VELOCITAT");
+
+        for(Map.Entry<String,Integer> entry : vehicleSeleccionat.getComponents().entrySet()){
+            System.out.printf("\n\t%-20s %-15d",
+                    entry.getKey(),entry.getValue());
+        }
+
+        System.out.print("\n\t------------------------------------------------------------\n\n");
+    }
+    private static void historialOrdres(){
+        // ORDRE ANTERIOR
+        System.out.print("\t-------------------------- ORDRES --------------------------");
+
+        for (int i = 0; i < TestCotxe_carlos_pomares.ORDRES.size(); i++) {
+            System.out.printf("\n\t%-10s %-15s",
+                    (i+1), TestCotxe_carlos_pomares.ORDRES.get(i));
+        }
+
+        System.out.print("\n\t------------------------------------------------------------\n\n");
+
+    }
+    private static void informeErrors(){
+        // Si hi ha errors en la conduccio surten
+        System.out.print("\t-------------------------- ERRORS --------------------------");
+
+        for (int i = 0; i < ERRORS.size(); i++) {
+            System.out.printf("\n\t%-10s %-15s",
+                    (i+1), ERRORS.get(i));
+        }
+
+        System.out.print("\n\t------------------------------------------------------------\n\n");
+    }
+
+    public static void main(String[] args) {
+        menuPrincipal();
+    }
 }
