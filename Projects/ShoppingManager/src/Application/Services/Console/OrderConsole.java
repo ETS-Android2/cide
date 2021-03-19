4b17143919035ca92e7bc0374a776b6d6b9697af
Carlos Pomares <45567247+pomaretta@users.noreply.github.com>
2021-03-19 10:22:10 +0100

    Added more comments on OrderConsole

--- Projects/ShoppingManager/src/Application/Services/Console/OrderConsole.java
+++ Projects/ShoppingManager/src/Application/Services/Console/OrderConsole.java
@@ -32,9 +32,19 @@ import java.util.Map;
 
 public class OrderConsole extends DefaultConsole {
 
-    private int MAX_ERRORS = 5;
-    private DatabaseDriver driver;
+    /**
+     * Driver para las consultas con la base de datos.
+     */
+    private final DatabaseDriver driver;
 
+    /**
+     *
+     *  Inicializa el driver para la base de datos,
+     *  establece el parser de comandos con esta consola e
+     *  intenta crear la conexión de forma automática
+     *  si ocurre algun error en la conexión, pide introducirla de manera manual.
+     *
+     */
     public OrderConsole() {
         driver = new DatabaseDriver();
         OrderParser.setConsole(this);
@@ -53,10 +63,12 @@ public class OrderConsole extends DefaultConsole {
 
     }
 
-    /*
-        MAIN MENU
-     */
 
+    /**
+     *
+     * Menu principal que conducira a otros menús.
+     *
+     */
     private void init(){
 
         String[] options = {
@@ -118,10 +130,12 @@ public class OrderConsole extends DefaultConsole {
 
     }
 
-    /*
-        CLIENT MENU
+    /**
+     *
+     * Menú cliente, que permite registrar nuevos clientes,
+     * buscar clientes por nombre y ver todos los clientes registrados.
+     *
      */
-
     private void client(){
 
         String[] options = {
@@ -179,6 +193,13 @@ public class OrderConsole extends DefaultConsole {
 
     }
 
+    /**
+     *
+     * Registro de nuevo cliente, menu sencuencial con validación que busca obtener
+     * todos los datos necesarios que de un cliente.
+     *
+     * @throws SQLException si a la hora de hacer el registro con la base de datos ocurre algún error.
+     */
     private void registerNewClient() throws SQLException {
 
         String[] messages = {
@@ -211,7 +232,6 @@ public class OrderConsole extends DefaultConsole {
         result.remove(result.get(result.size() - 1));
         result.add(i,n.replace("-",""));
 
-        // TODO Add driver update.
         driver.agregarCliente(new Client(
                 driver.obtenerNuevoIDCliente(),
                 result.get(0),
@@ -225,6 +245,14 @@ public class OrderConsole extends DefaultConsole {
 
     }
 
+    /**
+     *
+     * Pide a través de un menú secuencial un nombre de cliente, entonces pide al driver de base de datos
+     * un resultado de los clientes que coincidan.
+     *
+     * @return un listado de clientes.
+     * @throws Exception si a la hora de hacer la consulta con la base de datos ocurre algún error.
+     */
     private ArrayList<Client> searchClientByName() throws Exception {
 
         String[] messages = {
@@ -242,6 +270,16 @@ public class OrderConsole extends DefaultConsole {
         return (ArrayList<Client>) driver.buscarClientePorNombre(result.get(0));
     }
 
+    /**
+     *
+     * Permite ver de forma paginada a los clientes y obtener un el cliente seleccionado si la opción de selección
+     * esta activada.
+     *
+     * @param clients el listado de clientes.
+     * @param selection si el menu permite la selección de un cliente.
+     * @return null si no hay opción de selección, un cliente si la hay.
+     * @throws Exception si ocurre algún error.
+     */
     private Client clientView(ArrayList<Client> clients,boolean selection) throws Exception {
 
         final Client[] selected = new Client[1];
@@ -348,10 +386,12 @@ public class OrderConsole extends DefaultConsole {
         return selected[0];
     }
 
-    /*
-        PRODUCT MENU
+    /**
+     *
+     * Menú producto, permite registrar un nuevo producto,
+     * buscar productos por título y ver todos los productos.
+     *
      */
-
     private void product(){
 
         String[] options = {
@@ -409,6 +449,13 @@ public class OrderConsole extends DefaultConsole {
 
     }
 
+    /**
+     *
+     * Menú secuencial que pide todos los parámetros necesarios
+     * para crear un producto y registrarlo en la base de datos.
+     *
+     * @throws Exception si ocurre alguna excepción en el proceso.
+     */
     private void registerNewProduct() throws Exception {
 
         String[] messages = {
@@ -442,6 +489,14 @@ public class OrderConsole extends DefaultConsole {
 
     }
 
+    /**
+     *
+     * Menú secuencial que pide el título del producto a encontrar
+     * y obtiene un listado de objetos tipo Product.
+     *
+     * @return un ArrayList de tipo Product.
+     * @throws Exception si ocurre algún error en el proceso.
+     */
     private ArrayList<Product> searchProductByTitle() throws Exception {
 
         String[] messages = {
@@ -459,6 +514,16 @@ public class OrderConsole extends DefaultConsole {
         return (ArrayList<Product>) driver.buscarProductoPorTitulo(result.get(0));
     }
 
+    /**
+     *
+     * Permite ver los productos de forma paginada y si la opción de selección esta habilitado
+     * permite seleccionar un producto y devolverlo.
+     *
+     * @param products el listado de productos a mostrar.
+     * @param selection si esta habilitada la selección.
+     * @return null si la selección esta deshabilitada, sino devuelve un objeto de tipo Product.
+     * @throws Exception si ocurre alguna excepción en la ejecución.
+     */
     private Product productView(ArrayList<Product> products, boolean selection) throws Exception {
 
         final Product[] selected = new Product[1];
@@ -561,10 +626,12 @@ public class OrderConsole extends DefaultConsole {
         return selected[0];
     }
 
-    /*
-        ORDER MENU
+    /**
+     *
+     * Menú de Encargos, permite crear nuevos encargos,
+     * buscar encargos por cliente y eliminar un encargo.
+     *
      */
-
     private void order(){
 
         String[] options = {
@@ -622,6 +689,13 @@ public class OrderConsole extends DefaultConsole {
 
     }
 
+    /**
+     *
+     * Menú secuencial que permite realizar un encargo, en primer lugar pide a traves del clientView un cliente,
+     * y después establece un menú interactivo el cual permite agregar productos.
+     *
+     * @throws Exception si ocurre algún error.
+     */
     private void registerNewOrder() throws Exception {
         Client selectedClient = clientView((ArrayList<Client>) driver.obtenerTodosLosClientes(),true);
         HashMap<Product,Integer> products = productCart();
@@ -633,19 +707,38 @@ public class OrderConsole extends DefaultConsole {
         updateProducts(products);
     }
 
+    /**
+     *
+     * Menú secuencial, que pide la selección de una orden a eliminar y manda la solicitud al driver de bases de datos.
+     *
+     * @throws Exception si ocurre algún error.
+     */
     private void removeOrder() throws Exception {
         Order order = orderView(driver.obtenerTodosLosEncargos(),true);
         assert order != null;
         driver.eliminarEncargo(order.getId());
     }
 
+    /**
+     *
+     * Permite realizar una busqueda de órdenes por cierto cliente que se obtendra a través de la selección.
+     *
+     * @throws Exception si ocurre algún error.
+     */
     private void searchOrderByClient() throws Exception {
         Client selected = clientView((ArrayList<Client>) driver.obtenerTodosLosClientes(),true);
         assert selected != null;
         orderView(driver.obtenerEncargosDeUnCliente(selected),false);
     }
 
-    private HashMap<Product,Integer> productCart() throws Exception {
+    /**
+     *
+     * Se visualiza un "carrito" en el cual permite agregar productos y sus respectivas cantidades,
+     * de forma que cuando se sale guardando las cantidades se devuelve un HashMap de los resultados.
+     *
+     * @return un listado de productos con sus cantidades.
+     */
+    private HashMap<Product,Integer> productCart() {
 
         HashMap<Product,Integer> products = new HashMap<>();
 
@@ -755,10 +848,28 @@ public class OrderConsole extends DefaultConsole {
         return result.get(0);
     }
 
+    /**
+     *
+     * Permite validar un producto de forma individual, comprueba si la cantidad pasada por parámetros
+     * es inferior o igual a la cantidad que obtenga por parte del stock en momento de ejecución.
+     *
+     * @param p el producto a encontrar la cantidad
+     * @param quantity la cantidad a comprobar
+     * @return si el producto es válido y cumple con el requisito.
+     * @throws Exception si ocurre algún problema a la hora de obtener los datos de la BBDD.
+     */
     private boolean validateProduct(Product p,int quantity) throws Exception {
         return driver.obtenerProductoPorId(p.getId()).getStock() >= quantity;
     }
 
+    /**
+     *
+     * Permite validar de forma colectiva un listado de productos, el objetivo es que todos sean válidos.
+     *
+     * @param p el listado de productos a validar.
+     * @return si el listado completo cumple con los requisitos.
+     * @throws Exception si algún producto no es válido.
+     */
     private boolean validateProducts(HashMap<Product,Integer> p) throws Exception {
         int count = 0;
         for(Map.Entry<Product,Integer> product : p.entrySet()){
@@ -775,12 +886,29 @@ public class OrderConsole extends DefaultConsole {
         return count == p.size();
     }
 
+    /**
+     *
+     * Permite actualizar el stock de los productos con el listado, a cada producto de la lista
+     * actualizará sus respectivas cantidades. Siempre usar despues de haber validado los productos.
+     *
+     * @param p el listado a actualizar.
+     * @throws Exception si ocurre alguna excepción con la base de datos.
+     */
     private void updateProducts(HashMap<Product,Integer> p) throws Exception {
         for(Map.Entry<Product,Integer> product : p.entrySet()){
             driver.updateProductStockDecrement(product.getKey(),product.getValue());
         }
     }
 
+    /**
+     *
+     * Permite visualizar los encargos y en caso de habilitar la selección, devolvera la orden seleccionada.
+     *
+     * @param orders el listado de encargos.
+     * @param selection si la selección está habilitada.
+     * @return null si esta deshabilitada la selección, el encargo si lo está.
+     * @throws Exception si ocurre algún error en el proceso.
+     */
     private Order orderView(ArrayList<Order> orders, boolean selection) throws Exception {
 
         final Order[] selected = new Order[1];
@@ -889,10 +1017,11 @@ public class OrderConsole extends DefaultConsole {
         return selected[0];
     }
 
-    /*
-        COMANDOS
+    /**
+     *
+     * Comando help, muestra los diferentes comandos.
+     *
      */
-
     public void help(){
 
         String[] comandos = {
@@ -909,6 +1038,12 @@ public class OrderConsole extends DefaultConsole {
         optionMenu.show();
 
     }
+
+    /**
+     *
+     * Comando author, muestra un mensaje del autor del programa.
+     *
+     */
     public void author(){
         System.out.printf("\n\t========= %s =========\n","Créditos del autor");
         System.out.print("\t   ______________________________\n" +
@@ -933,9 +1068,23 @@ public class OrderConsole extends DefaultConsole {
                 "\t    |  /                            /.\n" +
                 "\t    \\_/____________________________/.\n\n");
     }
+
+    /**
+     *
+     * Comando reset, permite vaciar los errores.
+     *
+     */
     public void reset(){
         errorLog.clear();
     }
+
+    /**
+     *
+     * Comando configure, muestra un menu secuencial que permite establecer una nueva conexión con
+     * la base de datos.
+     *
+     * @throws Exception si ocurre algún error a la hora de conectar.
+     */
     public void configure() throws Exception {
 
         String[] messages = {
