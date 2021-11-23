12418f7c1c9644a167a72682d4604c4898254731
Carlos Pomares <cpomaresp@gmail.com>
2021-11-23 20:09:16 +0100

    Removed semaphore

--- modules/services-22/exams/1eva_carlos_pomares/src/Cima.java
+++ modules/services-22/exams/1eva_carlos_pomares/src/Cima.java
@@ -1,14 +1,11 @@
 import java.util.PriorityQueue;
-import java.util.concurrent.Semaphore;
 
 public class Cima {
  
-    private Semaphore semaphore;
     // Lista de escaladores a recoger
     private PriorityQueue<Integer> escaladoresPendientes;
 
     public Cima() {
-        this.semaphore = new Semaphore(1);
         this.escaladoresPendientes = new PriorityQueue<Integer>();
     }
 
@@ -18,7 +15,7 @@ public class Cima {
      * 
      * @return Cantidad de escaladores pendientes.
      */
-    public synchronized Integer getEscaladoresPendientes() {
+    public Integer getEscaladoresPendientes() {
         return escaladoresPendientes.size();
     }
 
@@ -28,7 +25,7 @@ public class Cima {
      * 
      * @return True si quedan escaladores en la cima.
      */
-    public synchronized boolean isEscaladoresPendientes() {
+    public boolean isEscaladoresPendientes() {
         return !escaladoresPendientes.isEmpty();
     }
     
@@ -38,15 +35,8 @@ public class Cima {
      * 
      * @param escalador El escalador a añadir.
      */
-    public void nuevoEscalador(Escalador escalador) {
-        try {
-            semaphore.acquire();
-            // Un escalador representa a una persona.
-            escaladoresPendientes.add(1);
-            semaphore.release();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public synchronized void nuevoEscalador(Escalador escalador) {
+        escaladoresPendientes.add(1);
     }
 
     /**
@@ -55,10 +45,8 @@ public class Cima {
      * 
      * @return El escalador que se ha sacado de la cima.
      */
-    public void recogerEscaladores(Helicoptero helicoptero) throws Exception{
+    public synchronized void recogerEscaladores(Helicoptero helicoptero) throws Exception{
         if (isEscaladoresPendientes()) {
-            // Obtenemos el paso para poder sacar un escalador.
-            semaphore.acquire();
             // Avisamos a base que recogemos un escalador.
             System.out.println(
                 String.format(
@@ -80,8 +68,6 @@ public class Cima {
             }
             // Simulamos que tarda entre 15 y 20 segundos en recoger los pasajeros.
             Thread.sleep((int) (Math.random() * 20000) + 15000);
-            // Permitimos el paso al siguiente helicóptero.
-            semaphore.release();
         }
     }
 
